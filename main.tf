93ace935d36cece18971cd3a02237523a79cf2aa
Sachin-fdo <sachindra.fd@gmail.com>
2025-12-30 20:54:46 +0100

    Code Update

--- main.tf
+++ main.tf
@@ -1,1 +1,63 @@
+# Specify Terraform version
+terraform {
+  required_version = ">= 1.14.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure AWS Provider
+provider "aws" {
+  region = var.aws_region
+}
+
+# Create an S3 bucket
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = var.s3_bucket_name
+  acl    = "private"
+}
+
+# Create a Security Group
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow SSH and HTTP"
+  
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+  
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Launch an EC2 instance
+resource "aws_instance" "web" {
+  ami           = var.ami_id
+  instance_type = "t2.micro"
+  key_name      = var.key_name
+  security_groups = [aws_security_group.web_sg.name]
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}
 
--- variables.tf
+++ variables.tf
@@ -1,1 +1,25 @@
+variable "aws_region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "my_ip" {
+  description = "Your public IP for SSH access"
+  default     = "83.64.155.206"
+}
+
+variable "ami_id" {
+  description = "AMI ID for Ubuntu 22.04"
+  default     = "ami-0aeeebd8d2ab47354" # Update for your region if needed
+}
+
+variable "key_name" {
+  description = "EC2 key pair name"
+  default     = "ec2-keypair"
+}
+
+variable "s3_bucket_name" {
+  description = "Name of S3 bucket"
+  default     = "my-example-cloud-project-terra-infra-eu-aut-1" # Replace with unique name
+}
 
